ac888a8801e9bbdda628dbeef9bc8516647499d8
Raymond John Hill <raymondjohnhill@gmail.com>
2013-01-05 23:21:44 -0500

    changed from js to c#

---
+++ Assets/scripts/auto_destroy.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class auto_destroy : MonoBehaviour {
+	
+	public double deathTimer;
+	
+	// Use this for initialization
+	void Start () {
+		this.deathTimer = 1.0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/scripts/auto_spawn.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class auto_spawn : MonoBehaviour {
+	
+	public GameObject[] spawnList;
+	public double spawnRadius = 1.0;
+	private double spawnTimer = 1.0;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
